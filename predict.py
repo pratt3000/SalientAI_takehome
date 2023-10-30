e2b526029f28f77742727e83168b77fe0d15a0f6
pratt3000 <pratt3000@gmail.com>
2023-10-30 13:35:50 -0500

    reformatting

--- predict.py
+++ predict.py
@@ -5,10 +5,6 @@ import en_core_web_lg
 
 from transformers import pipeline
 
-nlp = en_core_web_lg.load()
-model_id = "google/flan-t5-base"
-model_name_on_hub = "Salient_ai" + model_id.split("/")[1]
-model_path = "pratt3000/" + model_name_on_hub
 
 def check_entities_in_conversation(transcript):
 
@@ -16,14 +12,19 @@ def check_entities_in_conversation(transcript):
     if transcript.startswith(starter):
         transcript = transcript[len(starter):]
 
-    doc = nlp(transcript)
+    doc = model_entity_rec(transcript)
     entity_types = [X.label_ for X in doc.ents]
     if list(set(entity_types) & set(['DATE', 'CARDINAL'])) == []:
         return False
     return True
 
 
-model = pipeline(model = model_path)
+model_id = "google/flan-t5-base"
+model_name_on_hub = "Salient_ai" + model_id.split("/")[1]
+model_path = "pratt3000/" + model_name_on_hub
+
+model_entity_rec = en_core_web_lg.load()
+model_nlp = pipeline(model = model_path)
 
 conversation = "Agent: Can you advise when you'll manage to make the payment? \nCustomer: I should be able to do it on Thursday next week."
 base_prompt = "Given the above transcript and today's day and date, give me the date when the customer is expected to make their payment in the format 'dd/mm/yyyy'. Return 'NA' if the conversation doesnt make sense and the customer hasnt promised of any dates. Just output the date in your response and nothing else."
@@ -32,7 +33,7 @@ prompt = conversation + "\nToday's Date: " + date + '\n' + base_prompt
 
 start = time.time()
 if check_entities_in_conversation(conversation):
-    res = model(prompt)
+    res = model_nlp(prompt)
 else:
     res = [{'generated_text': 'NA'}]
 
